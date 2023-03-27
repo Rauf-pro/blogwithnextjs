a6e308cbc47d7fcdda0728a7c87deb2558d44c44
rrauf8178 <rrauf8178@gmail.com>
2023-03-28 01:21:28 +0400

    Added Footer and styled

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
+import Newsletter from "./_child/Newsletter";
 
 export default function Footer() {
   return (
     <footer className="bg-gray-50">
+      <Newsletter />
       <div className="container mx-auto flex justify-center py-12">
         <div className="py-5">
           <div className="flex gap-6 justify-center">
@@ -18,7 +20,10 @@ export default function Footer() {
               <ImYoutube color="#888888" />
             </Link>
           </div>
-          <p className="py-5 text-gray-400">Copyright @2023 All rights reserved | This template is made by Rauf Rzayev </p>
+          <p className="py-5 text-gray-400">
+            Copyright @2023 All rights reserved | This template is made by Rauf
+            Rzayev{" "}
+          </p>
           <p className="text-gray-400 text-center">Terms & Condition</p>
         </div>
       </div>
